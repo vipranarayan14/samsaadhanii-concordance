ef3d084010aea101bbcbcd8f7df4eca27135f2c1
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2022-10-06 00:13:47 +0530

    chore(repo): init repo

---
+++ notes.md
@@ -0,0 +1,7 @@
+# To do
+
+1. x fix divider
+2. make dhatu div separate in mobile view
+3. try using grid
+4. try using service worker for sorting / filtering 
+5. x seperate sortby from search box
---
+++ script.js
@@ -0,0 +1,191 @@
+const listEle = document.querySelector("#app .list");
+const filterFormEle = document.querySelector("#app .filter-form");
+const sortSelectEle = document.querySelector("#app .sort-select");
+const searchInputEle = document.querySelector("#app .search-input");
+
+const ganas = {
+  भ्वादिः: 1,
+  अदादिः: 2,
+  जुहोत्यादिः: 3,
+  दिवादिः: 4,
+  स्वादिः: 5,
+  तुदादिः: 6,
+  रुधादिः: 7,
+  तनादिः: 8,
+  क्र्यादिः: 9,
+  चुरादिः: 10,
+};
+
+const createItem = (dhatuDetails) =>
+  `<div class="item">
+    <div class="dhatu-details">
+      <div class="dhatu-container">
+        <span class="dhatu">${dhatuDetails.dhatu}</span>
+        <span>${dhatuDetails.meaning}</span>
+      </div>
+      <div>${dhatuDetails.gana}</div>
+      <div>${dhatuDetails.it}</div>
+      <div>${dhatuDetails.padi}</div>
+    </div>
+    <div class="vritti-details">
+      <div>
+        <div class="vritti-name">माधवीयधातुवृत्तिः</div>
+        <div>${dhatuDetails.madhaviyaId}</div>
+      </div>
+      <div>
+        <div class="vritti-name">क्षीरतरङ्गिणी</div>
+        <div>${dhatuDetails.kshirataranginiId}</div>
+      </div>
+      <div>
+        <div class="vritti-name">धातुप्रदीपः</div>
+        <div>${dhatuDetails.dhatupradipaId}</div>
+      </div>
+    </div>
+   </div>`;
+
+const sortByArtha = (dhatuList) =>
+  [...dhatuList].sort((dhatuDetailsA, dhatuDetailsB) =>
+    new Intl.Collator().compare(dhatuDetailsA.meaning, dhatuDetailsB.meaning)
+  );
+
+const sortByGana = (dhatuList) =>
+  [...dhatuList].sort(
+    (dhatuDetailsA, dhatuDetailsB) =>
+      ganas[dhatuDetailsA.gana] - ganas[dhatuDetailsB.gana]
+  );
+
+const sortDhatupatha = (dhatuList, sortBy) => {
+  if (sortBy === "gana") {
+    if (dhatupathaSortedByGana) return dhatupathaSortedByGana;
+
+    dhatupathaSortedByGana = sortByGana(dhatuList);
+
+    return dhatupathaSortedByGana;
+  }
+
+  if (sortBy === "artha") {
+    if (dhatupathaSortedByArtha) return dhatupathaSortedByArtha;
+
+    dhatupathaSortedByArtha = sortByArtha(dhatuList);
+
+    return dhatupathaSortedByArtha;
+  }
+
+  return dhatuList;
+};
+
+const filterDhatupatha = (dhatuList, keywordsSets) =>
+  dhatuList.filter((dhatuDetails) => {
+    const { dhatu, meaning, gana, padi, it } = dhatuDetails;
+    const detailsStr = [dhatu, meaning, gana, padi, it].join(" ");
+
+    return keywordsSets.some((keywordsSet) =>
+      keywordsSet.every((keyword) => detailsStr.includes(keyword))
+    );
+  });
+
+const getFilterKeywords = (searchStr) => {
+  const keywordsSets = [];
+
+  keywordsSets.push(searchStr.split(" "));
+  keywordsSets.push(Sanscript.t(searchStr, "wx", "devanagari").split(" "));
+  keywordsSets.push(Sanscript.t(searchStr, "itrans", "devanagari").split(" "));
+
+  return keywordsSets;
+  // .split(" ").filter((keyword) => !!keyword);
+};
+
+class List {
+  constructor(element) {
+    this.element = element;
+    this.itemsToLoad = 20;
+
+    this.list = [];
+    this.lastLoadedId = 0;
+
+    this.observer = new IntersectionObserver((entries) =>
+      this.onIntersect(entries)
+    );
+  }
+
+  setData(list) {
+    this.list = list;
+    this.lastLoadedId = 0;
+    this.clearContent();
+    this.loadNext();
+  }
+
+  setContent(fromId, toId) {
+    const content = this.list.slice(fromId, toId).map(createItem).join("");
+
+    this.element.insertAdjacentHTML("beforeend", content);
+
+    this.lastLoadedId = toId;
+  }
+
+  clearContent() {
+    this.element.innerHTML = "";
+  }
+
+  stopObserver() {
+    if (this.element.lastElementChild) {
+      this.observer.unobserve(this.element.lastElementChild);
+    }
+  }
+
+  startObserver() {
+    this.observer.observe(this.element.lastElementChild);
+  }
+
+  loadNext() {
+    this.stopObserver();
+
+    if (this.lastLoadedId > this.list.length - 1) return;
+
+    this.setContent(this.lastLoadedId, this.lastLoadedId + this.itemsToLoad);
+
+    this.startObserver();
+  }
+
+  onIntersect(entries) {
+    entries.forEach((entry) => entry.isIntersecting && this.loadNext());
+  }
+}
+
+let dhatupatha = [],
+  dhatupathaSortedByGana,
+  dhatupathaSortedByArtha;
+
+const list = new List(listEle);
+
+(async () => {
+  const result = await fetch("dhatupatha.json");
+
+  const data = await result.json();
+
+  dhatupatha = data;
+
+  list.setData(dhatupatha);
+})();
+
+filterFormEle.addEventListener("submit", (e) => e.preventDefault());
+
+sortSelectEle.addEventListener("change", (e) => {
+  const sortBy = e.target.value;
+
+  const sortedList = sortDhatupatha(dhatupatha, sortBy);
+
+  list.setData(sortedList);
+});
+
+searchInputEle.addEventListener("input", (e) => {
+  const searchStr = searchInputEle.value;
+
+  if (!searchStr) return list.setData(dhatupatha);
+
+  const keywords = getFilterKeywords(searchStr);
+
+  const filteredList = filterDhatupatha(dhatupatha, keywords);
+
+  list.setData(filteredList);
+});
