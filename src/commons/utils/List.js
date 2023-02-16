a807ba47676ba24fa9c433a028cfb46a021f6e05
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2023-02-16 16:49:43 +0000

    refactor: make private fields private in `List`
    What?
    Use '#' to make private-intended properties and methods actually
    private.
    
    Why?
    - To clearly indicate which fields are meant for private use and which
    are for public.
    - To avoid accidently using private-intended fields outside of the
    class.
    
    Note:
    Private fields are already supported in all the recent browsers
    (about 90% of the global usage -- check caniuse).

--- src/commons/utils/List.js
+++ src/commons/utils/List.js
@@ -56,74 +56,70 @@ const focusItem = (item) => {
 };
 
 export class List {
+  #itemsToLoad = 20;
+  #lastLoadedId = 0;
+  #list = [];
+  #observer = new IntersectionObserver((entries) => this.#onIntersect(entries));
+
   constructor(element) {
     this.element = element;
-    this.itemsToLoad = 20;
-
-    this.list = [];
-    this.lastLoadedId = 0;
-
-    this.observer = new IntersectionObserver((entries) =>
-      this.onIntersect(entries)
-    );
-  }
-
-  setData(list) {
-    this.list = list;
-    this.lastLoadedId = 0;
-    this.clearContent();
-    this.loadNext();
-
-    scrollToTop();
-  }
-
-  setContent(fromId, toId) {
-    const items = this.list.slice(fromId, toId).map(createDhatuListItem);
-
-    this.element.append(...items);
-
-    this.lastLoadedId = toId;
-  }
-
-  clearContent() {
-    this.element.innerHTML = "";
-  }
-
-  stopObserver() {
-    if (this.element.lastElementChild) {
-      this.observer.unobserve(this.element.lastElementChild);
-    }
-  }
-
-  startObserver() {
-    this.observer.observe(this.element.lastElementChild);
-  }
-
-  loadNext() {
-    this.stopObserver();
-
-    if (this.lastLoadedId > this.list.length - 1) return;
-
-    this.setContent(this.lastLoadedId, this.lastLoadedId + this.itemsToLoad);
-
-    this.startObserver();
   }
 
   goToItem(itemId) {
-    const index = this.list.findIndex(({ id }) => id === itemId);
+    const index = this.#list.findIndex(({ id }) => id === itemId);
 
     if (index < 0) return;
 
-    this.setContent(0, index);
-
-    this.loadNext();
+    this.#append(0, index);
+    this.#loadNext();
 
     const item = qs(`[data-item-id="${itemId}"]`, this.element);
 
     focusItem(item);
   }
 
-  onIntersect(entries) {
-    entries.forEach((entry) => entry.isIntersecting && this.loadNext());
+  setData(list) {
+    this.#list = list;
+    this.#lastLoadedId = 0;
+    this.#clear();
+    this.#loadNext();
+
+    scrollToTop();
+  }
+
+  #append(fromId, toId) {
+    const items = this.#list.slice(fromId, toId).map(createDhatuListItem);
+
+    this.element.append(...items);
+
+    this.#lastLoadedId = toId;
+  }
+
+  #clear() {
+    this.element.innerHTML = "";
+  }
+
+  #stopObserver() {
+    if (this.element.lastElementChild) {
+      this.#observer.unobserve(this.element.lastElementChild);
+    }
+  }
+
+  #startObserver() {
+    this.#observer.observe(this.element.lastElementChild);
+  }
+
+  #onIntersect(entries) {
+    entries.forEach((entry) => entry.isIntersecting && this.#loadNext());
+  }
+
+  #loadNext() {
+    this.#stopObserver();
+
+    if (this.#lastLoadedId > this.#list.length - 1) return;
+
+    this.#append(this.#lastLoadedId, this.#lastLoadedId + this.#itemsToLoad);
+
+    this.#startObserver();
   }
 }
