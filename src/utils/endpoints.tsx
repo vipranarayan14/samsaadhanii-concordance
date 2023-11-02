93bdaaf658a66c7ba4da347ef93598dccfeb7874
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2023-11-02 13:33:22 +0000

    fix: use prod endpoints instead of dev

--- src/utils/endpoints.tsx
+++ src/utils/endpoints.tsx
@@ -1,9 +1,12 @@
+// const basePath = "http://prapi.local:5445";
+// const basePath = "http://localhost:5445";
+
 export const ENDPOINTS = {
   DHATUPATHA: "/dhatupatha.json",
-  // VRITTI: "https://cdn.jsdelivr.net/gh/samsaadhanii/scl/dhaatupaatha/files",
-  VRITTI: "http://172.18.0.1:4000/dhaatupaatha/files",
+  VRITTI: "https://cdn.jsdelivr.net/gh/samsaadhanii/scl/dhaatupaatha/files",
+  // VRITTI: `${basePath}/dhaatupaatha/files`,
   GRAPH: "https://cdn.jsdelivr.net/gh/samsaadhanii/scl/dhaatupaatha/graphs",
-  // GRAPH: "http://172.18.0.1:4000/dhaatupaatha/graphs",
-  // FORMS: "https://sanskrit.uohyd.ac.in//cgi-bin/scl/skt_gen/verb/verb_gen.cgi",
-  FORMS: "http://172.18.0.1:4000/cgi-bin//skt_gen/verb/verb_gen.cgi",
+  // GRAPH: `${basePath}/dhaatupaatha/graphs`,
+  FORMS: "https://sanskrit.uohyd.ac.in/cgi-bin/scl/skt_gen/verb/verb_gen.cgi",
+  // FORMS: `${basePath}/cgi-bin//skt_gen/verb/verb_gen.cgi`,
 };
