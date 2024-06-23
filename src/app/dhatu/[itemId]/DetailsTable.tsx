51994dd35bc19014461d59c1c6f6a7528d649f51
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2024-06-23 13:37:25 +0000

    feat: improve dhatu details look in dhatu page
    How?
    - Use cards instead of table.
    - Increase font size.

--- src/app/dhatu/[itemId]/DetailsTable.tsx
+++ src/app/dhatu/[itemId]/DetailsTable.tsx
@@ -17,88 +17,33 @@ type Props = {
 export function DhatuDetailsTable({ dhatuDetails }: Props) {
   const { dhatuId, muladhatu, dhatu, meaning, gana, padi, it } = dhatuDetails;
 
-  // return (
-  //   <Container fluid className="px-0" style={{ fontSize: "1.1rem" }}>
-  //     {" "}
-  //     <Row className="py-1">
-  //       <Col sm={2} className="fw-bold">
-  //         मूलधातुः
-  //       </Col>
-  //       <Col>{muladhatu}</Col>
-  //     </Row>
-  //     <Row className="py-1">
-  //       <Col sm={2} className="fw-bold">
-  //         धातुः
-  //       </Col>
-  //       <Col>{dhatu}</Col>
-  //     </Row>
-  //     <div className="row py-1">
-  //       <Col sm={2} className="fw-bold">
-  //         अर्थः
-  //       </Col>
-  //       <Col>{meaning}</Col>
-  //     </div>
-  //     <Row className="py-1">
-  //       <Col sm={2} className="fw-bold">
-  //         गणः
-  //       </Col>
-  //       <Col>{gana}</Col>
-  //     </Row>
-  //     <Row className="py-1">
-  //       <Col sm={2} className="fw-bold">
-  //         पदि
-  //       </Col>
-  //       <Col>{padi}</Col>
-  //     </Row>
-  //     <Row className="py-1">
-  //       <Col sm={2} className="fw-bold">
-  //         इट्
-  //       </Col>
-  //       <Col>{it}</Col>
-  //     </Row>
-  //   </Container>
-  // );
+  const LabelledDetails = [
+    { label: "मूलधातुः", content: muladhatu },
+    { label: "धातुः", content: dhatu },
+    { label: "अर्थः", content: meaning },
+    { label: "गणः", content: gana },
+    { label: "पदि", content: padi },
+    { label: "इट्", content: it },
+  ];
 
   return (
-    <Card className="_bg-surface _border-thick">
-      <Card.Body>
-        <table
-          className={`${styles.detailsTable} _bg-surface text-center m-auto`}
-          style={{ width: "50%" }}
-        >
-          <tbody className="_bg-surface">
-            <tr>
-              <th>मूलधातुः</th>
-              <td>{muladhatu}</td>
-            </tr>
-            <tr>
-              <th>धातुः</th>
-              <td>{dhatu}</td>
-            </tr>
-            <tr>
-              <th>अर्थः</th>
-              <td>{meaning}</td>
-            </tr>
-            <tr>
-              <th>गणः</th>
-              <td>{gana}</td>
-            </tr>
-            <tr>
-              <th>पदि</th>
-              <td>{padi}</td>
-            </tr>
-            <tr>
-              <th>इट्</th>
-              <td>{it}</td>
-            </tr>
-            {/* TODO: Show dhatuId only under "Advanced" mode */}
-            {/* <tr>
-              <th>dhatuId</th>
-              <td>{dhatuId}</td>
-            </tr> */}
-          </tbody>
-        </table>
-      </Card.Body>
-    </Card>
+    <Row xs={2} md={3} className="g-4">
+      {LabelledDetails.map(({ label, content }) => (
+        <Col key={label}>
+          <Card
+            bg="white"
+            className="border _border-divider _border-thick text-center"
+          >
+            <Card.Header className="_border-thick fs-6 fw-bold">
+              {label}
+            </Card.Header>
+            <Card.Body>
+              {/* <Card.Title>Special title treatment</Card.Title> */}
+              <Card.Text className="fs-5">{content}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   );
 }
